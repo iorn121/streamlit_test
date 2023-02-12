884a72cde88eb7eeeb377c565fe4d5ac2f9a7b60
iorn <wmt.tkn.121@gmail.com>
2023-02-13 01:20:02 +0900

    test

--- src/main.py
+++ src/main.py
@@ -16,11 +16,14 @@ st.set_page_config(
     initial_sidebar_state="auto",
 )
 
+st.text(glob('/Library/Fonts//*.ttf')[0] if os.name=="posix" else glob('C:\Windows\Fonts\*.ttf')[0])
+
 class Comment2WordCloud:
     def __init__(self, csv_path, header=None):
         self.__data = pd.read_csv(csv_path, header=None, encoding='utf-8')
 
         self.fpath =  glob('/Library/Fonts//*.ttf')[0] if os.name=="posix" else glob('C:\Windows\Fonts\*.ttf')[0]
+
 
         # カウントしない文節をsetで用意
         self.non_count = set(["/", "", " ", "　", "、", "。", ".",  "せる", "まし", "まし","ます","てる","たら"])
